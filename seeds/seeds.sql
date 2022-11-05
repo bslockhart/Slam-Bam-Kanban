dc0295196801305f66630e7f94eecfe8f4c2708e
Zack <zack@Zacks-MBP.lan>
2022-11-05 09:11:09 -0400

    started seeds

---
+++ seeds/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO task
+	(taskName,
+    class,
+	taskDescription,
+	taskDue,
+    )
+VALUES
+	("Finish Project 2",
+		"Class Work",
+		"Need to finish Project 2",
+		"2022-01-01",
+    );
